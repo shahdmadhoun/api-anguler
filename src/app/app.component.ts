f5f54d6ee8929102eff05016408ed666dcb7797d
shahdmadhoun <shahdem100@gmail.com>
2021-01-13 21:00:36 +0300

    solve google error

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import citysData from './citys.json';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  
+  citys: city[] = citysData;
+  cityCode = 'city';
+}
